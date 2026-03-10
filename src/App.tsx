6a1d4ba1b867893cbb6f5885ef85e0ea414d5f88
rhianna larocque <rhiannalarocque@rhiannas-mbp.lan>
2026-03-10 16:07:17 -0400

    add location fetch and placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 
 import { Sun } from "lucide-react"
-import { GetStartedForm } from "./components/get-started-card"
-import { useState } from "react"
+
+import { useEffect, useState } from "react"
+import { Spinner } from "./components/ui/spinner"
+
+type TLatLong = {
+  lat: number;
+  long: number
+}
 
 export function App() {
-  const [selectedZip, setSelectedZip] = useState<number | null>(null)
-  console.log({ selectedZip })
+  const [latLong, setLatLong] = useState<TLatLong | null>(null)
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos.coords)
+      setLatLong({
+        lat: pos.coords.latitude,
+        long: pos.coords.longitude
+      })
+      setIsLoading(false)
+    });
+  }, [])
+
   return (
-    <div className="flex flex-col min-h-svh p-6">
-      <div className="flex space-x-2 pb-6">
+    <div className="flex flex-col min-h-svh">
+      <div className="flex space-x-2 p-6 w-full h-full bg-accent">
         <h1 className="font-medium">Weather Up</h1>
         <Sun />
       </div>
-      <div className="flex justify-center w-full">
-        <GetStartedForm
-          className="w-full sm:max-w-[50%]"
-          selectedZip={selectedZip}
-          setSelectedZip={setSelectedZip}
-        />
+      <div className="flex flex-1 flex-col items-center justify-center w-full space-y-2">
+        {isLoading ? (
+          <>
+            <h1 className="text-secondary-foreground">Grabbing User Location</h1>
+            <Spinner />
+          </>
+        ) : (
+          <div>
+            <pre>lat: {latLong?.lat}</pre>
+            <pre>long: {latLong?.long}</pre>
+          </div>
+        )}
       </div>
     </div>
   )
